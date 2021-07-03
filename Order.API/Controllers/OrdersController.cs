1c231f84a65564d62285175318f73cb180c5cd12
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-07-04 01:03:17 +0300

    Order API (Microservice) -4

---
+++ Order.API/Controllers/OrdersController.cs
@@ -0,0 +1,72 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Order.API.DTOs;
+using Order.API.Models;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Order.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        private readonly IPublishEndpoint _publishEndpoint;
+
+        public OrdersController(AppDbContext context, IPublishEndpoint publishEndpoint)
+        {
+            _context = context;
+            _publishEndpoint = publishEndpoint;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(OrderCreateDto orderCreate)
+        {
+            var newOrder = new Models.Order
+            {
+                BuyerId = orderCreate.BuyerId,
+                Status = OrderStatus.Suspend,
+                Address = new Address { Line = orderCreate.Address.Line, Province = orderCreate.Address.Province, District = orderCreate.Address.District },
+                CreatedDate = DateTime.Now
+            };
+
+            orderCreate.orderItems.ForEach(item =>
+            {
+                newOrder.Items.Add(new OrderItem() { Price = item.Price, ProductId = item.ProductId, Count = item.Count });
+            });
+
+            await _context.AddAsync(newOrder);
+
+            await _context.SaveChangesAsync();
+
+            var orderCreatedEvent = new OrderCreatedEvent()
+            {
+                BuyerId = orderCreate.BuyerId,
+                OrderId = newOrder.Id,
+                Payment = new PaymentMessage
+                {
+                    CardName = orderCreate.payment.CardName,
+                    CardNumber = orderCreate.payment.CardNumber,
+                    Expiration = orderCreate.payment.Expiration,
+                    CVV = orderCreate.payment.CVV,
+                    TotalPrice = orderCreate.orderItems.Sum(x => x.Price * x.Count)
+                },
+            };
+
+            orderCreate.orderItems.ForEach(item =>
+            {
+                orderCreatedEvent.orderItems.Add(new OrderItemMessage { Count = item.Count, ProductId = item.ProductId });
+            });
+
+            await _publishEndpoint.Publish(orderCreatedEvent);
+
+            return Ok();
+        }
+    }
+}
---
+++ Order.API/DTOs/OrderCreateDto.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Order.API.DTOs
+{
+    public class OrderCreateDto
+    {
+        public string BuyerId { get; set; }
+        public List<OrderItemDto> orderItems { get; set; }
+        public PaymentDto payment { get; set; }
+        public AddressDto Address { get; set; }
+    }
+
+    public class OrderItemDto
+    {
+        public int ProductId { get; set; }
+        public int Count { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class PaymentDto
+    {
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public string Expiration { get; set; }
+        public string CVV { get; set; }
+    }
+
+    public class AddressDto
+    {
+        public string Line { get; set; }
+        public string Province { get; set; }
+        public string District { get; set; }
+    }
+}
