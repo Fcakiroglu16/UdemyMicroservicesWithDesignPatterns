7f06ffc9c5b7085264d19630c6113cfebfec7945
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2021-07-12 00:15:04 +0300

    EventSourcing 7.Adım

---
+++ EventSourcing.API/EventStores/ProductStream.cs
@@ -0,0 +1,39 @@
+﻿using EventSourcing.API.DTOs;
+using EventSourcing.Shared.Events;
+using EventStore.ClientAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventSourcing.API.EventStores
+{
+    public class ProductStream : AbstractStream
+    {
+        public static string StreamName => "ProductStream";
+
+        public ProductStream(IEventStoreConnection eventStoreConnection) : base(StreamName, eventStoreConnection)
+        {
+        }
+
+        public void Created(CreateProductDto createProductDto)
+        {
+            Events.AddLast(new ProductCreatedEvent { Id = Guid.NewGuid(), Name = createProductDto.Name, Price = createProductDto.Price, Stock = createProductDto.Stock, UserId = createProductDto.UserId });
+        }
+
+        public void NameChanged(ChangeProductNameDto changeProductNameDto)
+        {
+            Events.AddLast(new ProductNameChangedEvent { ChangedName = changeProductNameDto.Name, Id = changeProductNameDto.Id });
+        }
+
+        public void PriceChanged(ChangeProductPriceDto changeProductPriceDto)
+        {
+            Events.AddLast(new ProductPriceChangedEvent() { ChangedPrice = changeProductPriceDto.Price, Id = changeProductPriceDto.Id });
+        }
+
+        public void Deleted(Guid id)
+        {
+            Events.AddLast(new ProductDeletedEvent { Id = id });
+        }
+    }
+}
--- EventSourcing.API/Startup.cs
+++ EventSourcing.API/Startup.cs
@@ -34,6 +34,7 @@ namespace EventSourcing.API
             });
 
             services.AddEventStore(Configuration);
+            services.AddSingleton<ProductStream>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
